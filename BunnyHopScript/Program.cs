167baaf89c512c5d464cd2eeefce2ba8fc30e0ac
Feli <felinegri5@gmail.com>
2021-02-03 03:46:01 -0300

    First Version

---
+++ BunnyHopScript/Program.cs
@@ -0,0 +1,49 @@
+﻿using Memory;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Feli.BunnyHopScript
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(ushort virtualKeyCode);
+
+        private static Mem _mem = new Mem();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bunny Hop Script made by Feli (https://github.com/01-Feli/)");
+            Console.WriteLine("Loading Cheat...");
+            Console.WriteLine("Getting Process...");
+            Process csgo = Process.GetProcessesByName("csgo").FirstOrDefault();
+            while (csgo == null)
+            {
+                csgo = Process.GetProcessesByName("csgo").FirstOrDefault();
+            }
+
+            Console.WriteLine("Process is ready !");
+            _mem.OpenProcess(csgo.Id);
+            Thread bhopThread = new Thread(Bhop);
+            bhopThread.Start();
+        }
+
+        private static void Bhop()
+        {
+            Console.WriteLine("Cheat is ready !");
+            while (true)
+            {
+                if (GetAsyncKeyState(32) < 0)
+                {
+                    if (_mem.ReadInt("client.dll+0xD8A2BC,0x104", "") == 257)
+                    {
+                        _mem.WriteMemory("client.dll+0x524BE84", "int", "6");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BunnyHopScript/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BunnyHopScript")]
+[assembly: AssemblyDescription("A bunny hop script for Counter Strike")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BunnyHopScript")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("062e79b3-a2ea-4099-88e7-987483f55671")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
